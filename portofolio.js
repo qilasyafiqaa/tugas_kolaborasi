b331b490057434512272feecaeb0ea9a6f5deb7e
qilasyafiqaa <pikaaqilaa@gmail.com>
2026-05-16 14:56:38 +0700

    Add files via upload

---
+++ portofolio.js
@@ -0,0 +1,8 @@
+// Alert saat form dikirim
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+  e.preventDefault();
+
+  alert("Pesan berhasil dikirim!");
+ console.log("Portfolio Website Bootstrap Ready!");
+});
