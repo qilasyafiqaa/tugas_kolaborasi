d0d6f0ddcae088dc503f79803828f328a20727f2
qilasyafiqaa <pikaaqilaa@gmail.com>
2026-05-16 15:27:08 +0700

    Add files via upload

--- portofolio.js
+++ portofolio.js
@@ -1,8 +1,35 @@
-// Alert saat form dikirim
+// ================= CONTACT FORM =================
+
 document.getElementById("contactForm").addEventListener("submit", function(e){
 
   e.preventDefault();
 
   alert("Pesan berhasil dikirim!");
- console.log("Portfolio Website Bootstrap Ready!");
+
 });
+
+
+// ================= DARK MODE =================
+
+window.onload = function(){
+
+  const btn = document.getElementById("modeToggle");
+
+  btn.onclick = function(){
+
+    document.body.classList.toggle("dark-mode");
+
+    // Ganti icon
+    if(document.body.classList.contains("dark-mode")){
+
+      btn.innerHTML = "☀️";
+
+    }else{
+
+      btn.innerHTML = "🌙";
+
+    }
+
+  }
+
+}
